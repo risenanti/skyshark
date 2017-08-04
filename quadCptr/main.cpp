6ee88b077036b1bb16fc3ef567cb21a3499d9c1c
risenanti <risenanti@gmail.com>
2017-08-04 17:00:07 +0000

    testing 4 motors

--- quadCptr/main.cpp
+++ quadCptr/main.cpp
@@ -4,7 +4,11 @@
 #include "Navio/Util.h"
 #include <stdio.h>
 
-#define PWM_OUTPUT 0
+#define MOTOR1 0
+#define MOTOR2 1
+#define MOTOR3 2
+#define MOTOR4 3
+
 #define SERVO_MIN 1.250 /*MS*/
 #define SERVO_MAX 1.750 /*MS*/
 
@@ -16,40 +20,42 @@ int main()
 	if(check_apm()) { return 1;}
 
 	if (!led.initialize()) return EXIT_FAILURE;
-	if (!pwm.init(PWM_OUTPUT)) 
+	if (!(pwm.init(MOTOR1) && pwm.init(MOTOR2) && pwm.init(MOTOR3) && pwm.init(MOTOR4)))
 	{
 		fprintf(stderr,"OUTPUT ENABLE NOT SET. Are you root?\n");
 		return 0;
 	}
 
 	led.setColor(Colors::Yellow);
-	pwm.enable(PWM_OUTPUT);
-	pwm.set_period(PWM_OUTPUT,50);
+	pwm.enable(MOTOR1);
+	pwm.enable(MOTOR2);
+	pwm.enable(MOTOR3);
+	pwm.enable(MOTOR4);
+
+	pwm.set_period(MOTOR1,50);
+	pwm.set_period(MOTOR2,50);
+	pwm.set_period(MOTOR3,50);
+	pwm.set_period(MOTOR4,50);
 	
 	printf("Enable PWM\n");
-	pwm.set_duty_cycle(PWM_OUTPUT,SERVO_MIN);
-	sleep(15);
-	pwm.set_duty_cycle(PWM_OUTPUT,SERVO_MAX);
-	sleep(10);
-	int i = 5;
 
-	//while(i!=0)
-	//{
-		//pwm.set_duty_cycle(PWM_OUTPUT, SERVO_MAX);
-	//	led.setColor(Colors::Red);
-	//	printf("Servo HIGH\n");
-		//sleep(10);
+	while(true)
+	{
+		pwm.set_duty_cycle(MOTOR1, SERVO_MAX);
+		pwm.set_duty_cycle(MOTOR2, SERVO_MAX);
+		pwm.set_duty_cycle(MOTOR3, SERVO_MAX);
+		pwm.set_duty_cycle(MOTOR4, SERVO_MAX);
+		led.setColor(Colors::Red);
+		printf("Servo HIGH\n");
+		sleep(1);
 
-		//pwm.set_duty_cycle(PWM_OUTPUT,SERVO_MIN);
-	//	led.setColor(Colors::Blue);
-	//	printf("Servo LOW\n");
-		//sleep(10);
-	//	i--;
-	//}
-
-	pwm.set_duty_cycle(PWM_OUTPUT,0.000);
-	pwm.set_period(PWM_OUTPUT,0);
-	sleep(30);
-	printf("GOODBYE!\n");
+		pwm.set_duty_cycle(MOTOR1,SERVO_MIN);
+		pwm.set_duty_cycle(MOTOR2,SERVO_MIN);
+		pwm.set_duty_cycle(MOTOR3,SERVO_MIN);
+		pwm.set_duty_cycle(MOTOR4,SERVO_MIN);
+		led.setColor(Colors::Blue);
+		printf("Servo LOW\n");
+		sleep(1);
+	}
 	return 1;
 }
