daf1713025eacd8421d5fdcd774b9fcc8910e35c
risenanti <risenanti@gmail.com>
2017-09-27 19:03:02 -0500

    almost finished stabilization

--- src/test/src/main.cpp
+++ src/test/src/main.cpp
@@ -11,6 +11,11 @@
 #include "Navio/LSM9DS1.h"
 #include "Navio/PWM.h"
 
+#define MOTOR1 0
+#define MOTOR2 1
+#define MOTOR3 2
+#define MOTOR4 3
+
 // Radio min/max values for each stick for my radio (worked out at beginning of article)
 #define RC_THR_MIN   1070
 #define RC_YAW_MIN   1068
@@ -32,17 +37,42 @@ long map(long x, long in_min, long in_max, long out_min, long out_max)
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
+double constrain(double x, double a, double b) {
+    if(x < a) {
+        return a;
+    }
+    else if(b < x) {
+        return b;
+    }
+    else
+        return x;
+}
+
+
 #define wrap_180(x) (x < -180 ? x+360 : (x > 180 ? x - 360: x))
 
 // PID array (6 pids, two for each axis)
-double input,output,setpoint,kp,ki,kd;
-PID pids(&input,&output,&setpoint,(double)0.02,(double)1.0,(double)0.01, DIRECT);
+//double input,output,setpoint,kp,ki,kd;
 #define PID_PITCH_RATE 0
 #define PID_ROLL_RATE 1
 #define PID_PITCH_STAB 2
 #define PID_ROLL_STAB 3
 #define PID_YAW_RATE 4
 #define PID_YAW_STAB 5
+
+double input, output, setpoint;
+long rcthr, rcyaw, rcpit, rcroll;
+double pidPitchStab, pidPitchStabOut;
+double pidRollStab, pidRollStabOut;
+
+//PID pitchRate (&input, &output, &setpoint, 0.7 , 1.0, 0.0, DIRECT);
+//PID rollRate  (&input, &output, &setpoint, 0.7 , 1.0, 0.0, DIRECT);
+PID pitchStab (&pidPitchStab, &pidPitchStabOut, &setpoint, 4.5 , 0.0, 0.0, DIRECT);
+PID rollStab  (&pidRollStab, &pidRollStabOut, &setpoint, 0.0 , 0.0, 0.0, DIRECT);
+//PID yawRate   (&input, &output, &setpoint, 2.7 , 1.0, 0.0, DIRECT);
+//PID yawStab   (&input, &output, &setpoint, 10.0, 0.0, 0.0, DIRECT);
+
+PWM motors;
 
 int main(int argc, char **argv)
 {
@@ -56,27 +86,60 @@ int main(int argc, char **argv)
 	rcin.init();
 	sensor->initialize();
 	
-	float ax, ay, az;
-	float gx, gy, gz;
+	float gx; //ROLL
+	float gy; //PITCH 
+	float gz; //YAW
 	
-	/*pwm.enable(MOTOR1);
-	pwm.enable(MOTOR2);
-	pwm.enable(MOTOR3);
-	pwm.enable(MOTOR4);
+	motors.enable(MOTOR1);
+	motors.enable(MOTOR2);
+	motors.enable(MOTOR3);
+	motors.enable(MOTOR4);
 
-	pwm.set_period(MOTOR1,50);
-	pwm.set_period(MOTOR2,50);
-	pwm.set_period(MOTOR3,50);
-	pwm.set_period(MOTOR4,50);
-	*/
+	motors.set_period(MOTOR1,50);
+	motors.set_period(MOTOR2,50);
+	motors.set_period(MOTOR3,50);
+	motors.set_period(MOTOR4,50);
 	
 	while (1)
 	{
+		static double yaw_target = 0;  
 		sensor->update();
-	    sensor->read_accelerometer(&ax, &ay, &az);
+	    //shove gyroscope data straight into pid
         sensor->read_gyroscope(&gx, &gy, &gz);
 		/*array element 0 left stick up and down*/
-		volatile int rawInput1 = rcin.read(0); //read throttle from controller
+		volatile int rawInput1 = rcin.read(0); //throttle from controller
+		volatile int rawInput2 = rcin.read(1); //roll
+		volatile int rawInput3 = rcin.read(2); //pitch
+		volatile int rawInput4 = rcin.read(3); //yaw
+		
+		rcthr = rawInput1;
+		rcyaw = map(rawInput4, RC_YAW_MIN, RC_YAW_MAX, -180, 180);
+		rcpit = map(rawInput3, RC_PIT_MIN, RC_PIT_MAX, -45, 45);
+		rcroll = map(rawInput2, RC_ROL_MIN, RC_ROL_MAX, -45, 45);
+		double roll  = gx;
+		double pitch = gy;
+		double yaw   = gz; 	
+		
+		if(rcthr > RC_THR_MIN + 100) 
+		{  // Throttle raised, turn on stablisation.
+			pidPitchStab = rcpit-pitch;
+			pitchStab.Compute();
+			double piStabOut = constrain(pidPitchStabOut,-250,250);
+			
+			pidRollStab = rcroll - roll;
+			rollStab.Compute();
+			double piRollStabOut = constrain (pidRollStabOut, -250, 250);
+			
+		}
+		
+		else {
+			motors.set_duty_cycle(MOTOR1,1000);
+			motors.set_duty_cycle(MOTOR2,1000);
+			motors.set_duty_cycle(MOTOR3,1000);
+			motors.set_duty_cycle(MOTOR4,1000);
+			
+			yaw_target = yaw;
+		}
 		
 		usleep(1000);
 	}
