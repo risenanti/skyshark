e39605367b8a9ef546dacf3c121ff718028be240
Luc Lagarde <mlaga97@gmail.com>
2017-09-30 16:39:18 -0500

    Convert IMU publishers to use sensor_msgs/Imu

--- src/sensors_imu/imu.cpp
+++ src/sensors_imu/imu.cpp
@@ -1,9 +1,10 @@
-#include "iostream"
 #include "ros/ros.h"
 
 // Message headers
+#include "std_msgs/Header.h"
+#include "sensor_msgs/Imu.h"
 #include "geometry_msgs/Vector3.h"
-#include "sensors_imu/RawIMU_9.h"
+#include "geometry_msgs/Quaternion.h"
 
 // Navio headers
 #include "Navio/MPU9250.h"
@@ -11,94 +12,98 @@
 #include "Navio/Util.h"
 #include <unistd.h>
 
-int main(int argc, char **argv) {
+void imuPublisher(ros::Publisher& publisher, InertialSensor* sensor) {
 	// Variables
 	float ax, ay, az;
 	float gx, gy, gz;
 	float mx, my, mz;
 
+	// Create message object
+	sensor_msgs::Imu message;
+	
+	// Build the message
+	message.header = std_msgs::Header();
+	message.header.stamp = ros::Time::now();
+	
+	message.orientation = geometry_msgs::Quaternion();
+	message.orientation_covariance = {-1, 0, 0, 0, 0, 0, 0, 0, 0};
+	
+	message.angular_velocity = geometry_msgs::Vector3();
+	message.angular_velocity_covariance = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+	
+	message.linear_acceleration = geometry_msgs::Vector3();
+	message.linear_acceleration_covariance = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+	// Read IMU data
+	sensor->read_accelerometer(&ax, &ay, &az);
+	sensor->read_gyroscope(&gx, &gy, &gz);
+	sensor->read_magnetometer(&mx, &my, &mz);
+	
+	// Put gyro data in message
+	message.angular_velocity.x = gx;
+	message.angular_velocity.y = gy;
+	message.angular_velocity.z = gz;
+	
+	// Put accelerometer data in message
+	message.linear_acceleration.x = ax;
+	message.linear_acceleration.y = ay;
+	message.linear_acceleration.z = az;
+
+	// Publish message
+	publisher.publish(message);
+}
+
+/*
+void compassPublisher(ros::Publisher& publisher, InertialSensor* sensor) {
+	// Variables
+	float mx, my, mz;
+	
+	// Build the message
+	message.header = std_msgs::Header();
+	mpuMsg.compass_orientation = geometry_msgs::Vector3();
+
+	// Read IMU data (MPU)
+	sensor->read_magnetometer(&mx, &my, &mz);
+	
+	// Put magnometer data in message
+	message.compass_orientation.x = mx;
+	message.compass_orientation.y = my;
+	message.compass_orientation.z = mz;
+
+	// Publish message
+	publisher.publish(message);
+}
+
+void temperaturePublisher() {
+}
+*/
+
+int main(int argc, char **argv) {
 	// ROS Stuff
-	ros::init(argc, argv, "rcinputRaw");
+	ros::init(argc, argv, "imuRaw");
 	ros::NodeHandle n;
 	ros::Rate loop_rate(100);
 
 	// Create publishers
-	ros::Publisher mpuPub = n.advertise<sensors_imu::RawIMU_9>("mpuRaw", 1000);
-	ros::Publisher lsmPub = n.advertise<sensors_imu::RawIMU_9>("lsmRaw", 1000);
+	ros::Publisher mpuPublisher = n.advertise<sensor_msgs::Imu>("mpuRaw", 1000);
+	ros::Publisher lsmPublisher = n.advertise<sensor_msgs::Imu>("lsmRaw", 1000);
 	
 	// Sensor objects
 	InertialSensor *mpu = new MPU9250();
 	InertialSensor *lsm = new LSM9DS1();
-	
-	// TODO: "Probe" Sensors
 	
 	// Initialize
 	mpu->initialize();
 	lsm->initialize();
 
 	while(ros::ok()) {
-		// Create message objects
-		sensors_imu::RawIMU_9 mpuMsg;
-		sensors_imu::RawIMU_9 lsmMsg;
-		
-		// TODO: Add headers or something
-		// TODO: Better message objects
-		
-		// Create the message (MPU)
-		mpuMsg.angular_velocity = geometry_msgs::Vector3();
-		mpuMsg.linear_acceleration = geometry_msgs::Vector3();
-		mpuMsg.compass_orientation = geometry_msgs::Vector3();
-
-		// Read IMU data (MPU)
+		// MPU9250
 		mpu->update();
-		mpu->read_accelerometer(&ax, &ay, &az);
-		mpu->read_gyroscope(&gx, &gy, &gz);
-		mpu->read_magnetometer(&mx, &my, &mz);
+		imuPublisher(mpuPublisher, mpu);
 		
-		// Put gyro data in message (MPU)
-		mpuMsg.angular_velocity.x = gx;
-		mpuMsg.angular_velocity.y = gy;
-		mpuMsg.angular_velocity.z = gz;
-		
-		// Put accelerometer data in message (MPU)
-		mpuMsg.linear_acceleration.x = ax;
-		mpuMsg.linear_acceleration.y = ay;
-		mpuMsg.linear_acceleration.z = az;
-		
-		// Put magnometer data in message (MPU)
-		mpuMsg.compass_orientation.x = mx;
-		mpuMsg.compass_orientation.y = my;
-		mpuMsg.compass_orientation.z = mz;
-		
-		// Create the message (LSM)
-		lsmMsg.angular_velocity = geometry_msgs::Vector3();
-		lsmMsg.linear_acceleration = geometry_msgs::Vector3();
-		lsmMsg.compass_orientation = geometry_msgs::Vector3();
-
-		// Read IMU data (LSM)
+		// LSM9DS1
 		lsm->update();
-		lsm->read_accelerometer(&ax, &ay, &az);
-		lsm->read_gyroscope(&gx, &gy, &gz);
-		lsm->read_magnetometer(&mx, &my, &mz);
-		
-		// Put gyro data in message (LSM)
-		lsmMsg.angular_velocity.x = gx;
-		lsmMsg.angular_velocity.y = gy;
-		lsmMsg.angular_velocity.z = gz;
-		
-		// Put accelerometer data in message (LSM)
-		lsmMsg.linear_acceleration.x = ax;
-		lsmMsg.linear_acceleration.y = ay;
-		lsmMsg.linear_acceleration.z = az;
-		
-		// Put magnometer data in message (LSM)
-		lsmMsg.compass_orientation.x = mx;
-		lsmMsg.compass_orientation.y = my;
-		lsmMsg.compass_orientation.z = mz;		
-
-		// Publish messages
-		mpuPub.publish(mpuMsg);
-		lsmPub.publish(lsmMsg);
+		imuPublisher(lsmPublisher, lsm);
 		
 		// Other ROS maintenance things
 		ros::spinOnce();
