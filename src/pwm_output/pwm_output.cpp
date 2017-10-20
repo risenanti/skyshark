3339abe617fcdee2418f81ab2b7c552675b4ffef
risenanti <risenanti@gmail.com>
2017-10-20 00:29:05 +0000

    input to pwm_output needs fixing

---
+++ src/pwm_output/pwm_output.cpp
@@ -0,0 +1,134 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+
+#include "ros/ros.h"
+#include "std_msgs/Header.h"
+#include "std_msgs/Float32MultiArray.h"
+#include "skyshark_msgs/RotorSpeeds.h"
+
+#include "Navio/PWM.h"
+#include "Navio/Util.h"
+
+// Motor numbers definitions
+#define MOTOR_FL   0    // Front left    
+#define MOTOR_FR   1    // Front right
+#define MOTOR_BL   2    // back left
+#define MOTOR_BR   3    // back right
+
+class pwmOutput
+{
+	public:
+	pwmOutput();
+	void init(void);
+	void motorsOff(void);
+	void writeMotors(void);
+	void printStab(void);
+	void stabCallback(const skyshark_msgs::RotorSpeeds speed);
+	
+	private:
+	PWM motors;
+	
+	float stabOutput[4];
+
+};
+
+void pwmOutput::stabCallback(const skyshark_msgs::RotorSpeeds speed)
+{
+	stabOutput[0] = speed.rotor1;
+	stabOutput[1] = speed.rotor2;
+	stabOutput[2] = speed.rotor3;
+	stabOutput[3] = speed.rotor4;
+}
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "pwmOutput");
+	ros::NodeHandle n;
+	
+	ros::Rate loop_rate(100); /*100 HZ*/
+	
+	pwmOutput OUTPUT;
+	
+	if (check_apm()) {
+        return 1;
+    }
+    
+    OUTPUT.init();
+	
+	ros::Subscriber sub = n.subscribe("stabOut", 1000, &pwmOutput::stabCallback, &OUTPUT);
+	
+	while(true)
+	{
+		//OUTPUT.writeMotors();
+		OUTPUT.printStab();
+		ros::spinOnce();
+		loop_rate.sleep();
+	}
+	
+	
+	return 0;
+}
+
+void pwmOutput::init(void)
+{
+	
+	if(!motors.init(MOTOR_FL))
+	{
+		fprintf(stderr, "Output Enable not set. Are you root?\n");
+        return;
+	}
+	if(!motors.init(MOTOR_FR))
+	{
+		fprintf(stderr, "Output Enable not set. Are you root?\n");
+        return;
+	}
+	if (!motors.init(MOTOR_BL))
+	{
+		fprintf(stderr, "Output Enable not set. Are you root?\n");
+        return;
+	}
+	
+	if (!motors.init(MOTOR_BR))
+	{
+		fprintf(stderr, "Output Enable not set. Are you root?\n");
+        return;
+	}
+	
+	motors.enable(MOTOR_FL);
+	motors.enable(MOTOR_FR);
+	motors.enable(MOTOR_BL);
+	motors.enable(MOTOR_BR);
+
+	motors.set_period(MOTOR_FL,50);
+	motors.set_period(MOTOR_FR,50);
+	motors.set_period(MOTOR_BL,50);
+	motors.set_period(MOTOR_BR,50);
+}
+
+pwmOutput::pwmOutput()
+{
+	init();
+}
+
+void pwmOutput::motorsOff(void)
+{
+	motors.set_duty_cycle(MOTOR_FL,1.000);
+	motors.set_duty_cycle(MOTOR_FR,1.000);
+	motors.set_duty_cycle(MOTOR_BL,1.000);
+	motors.set_duty_cycle(MOTOR_BR,1.000);
+}
+
+void pwmOutput::writeMotors(void)
+{
+	motors.set_duty_cycle(MOTOR_FL, 1.200);//stabOutput[0]);
+	motors.set_duty_cycle(MOTOR_BL, 1.200);//stabOutput[1]);
+	motors.set_duty_cycle(MOTOR_FR, 1.200);//stabOutput[2]);
+	motors.set_duty_cycle(MOTOR_BR, 1.200);//stabOutput[3]);
+}
+
+void pwmOutput::printStab(void)
+{
+	printf("%f %f %f %f \n", stabOutput[0], stabOutput[1], stabOutput[2], stabOutput[3]);
+}
--- src/stabilize/stabilize.cpp
+++ src/stabilize/stabilize.cpp
@@ -7,7 +7,9 @@
 #include <unistd.h>
 #include <math.h>
 #include "ros/ros.h"
+#include "std_msgs/Header.h"
 #include "std_msgs/Float32MultiArray.h"
+#include "skyshark_msgs/RotorSpeeds.h"
 
 #include "stabHeaders/stabClass.h"
 
@@ -22,22 +24,47 @@ int main(int argc, char **argv)
 	ros::Subscriber nSub = s.subscribe("mpuRaw", 1000, &stabClass::mpuCallback, &stabilize);
 	
 	/*publisher setup*/
-	std_msgs::Float32MultiArray sendOutPWM;
+	skyshark_msgs::RotorSpeeds sendOutPWM;
 	ros::Rate loop_rate(100); /*100 HZ*/
 	ros::NodeHandle z;
-	ros::Publisher pwmOut = z.advertise<std_msgs::Float32MultiArray>("stabOutput", 1000);
+	ros::Publisher pwmOut = z.advertise<skyshark_msgs::RotorSpeeds>("stabOutput", 1000);
 	
 	while(1)
 	{
-		sendOutPWM.data.clear();
-		sendOutPWM.data.push_back(stabilize.getRCout1());
-		sendOutPWM.data.push_back(stabilize.getRCout2());
-		sendOutPWM.data.push_back(stabilize.getRCout3());
-		sendOutPWM.data.push_back(stabilize.getRCout4());
+		static int i = 0;
+		
+		//sendOutPWM.data.clear();
+		//sendOutPWM.rotor1 = stabilize.getRCout1();
+		//sendOutPWM.rotor2 = stabilize.getRCout2();
+		//sendOutPWM.data.push_back(stabilize.getRCout3());
+		//sendOutPWM.data.push_back(stabilize.getRCout4());
+		
+		if (i>500)
+		{
+			sendOutPWM.rotor1 = 1.500;
+			sendOutPWM.rotor2 = 1.500;
+			sendOutPWM.rotor3 = 1.500;
+			sendOutPWM.rotor4 = 1.500;
+			
+			if (i>800)
+			{
+				i = 0;
+			}
+		}
+		
+		else
+		
+		{
+			sendOutPWM.rotor1 = 1.200;
+			sendOutPWM.rotor2 = 1.200;
+			sendOutPWM.rotor3 = 1.200;
+			sendOutPWM.rotor4 = 1.200;
+		}
 		
 		pwmOut.publish(sendOutPWM);
-		ros::spin();	
+		ros::spinOnce();	
 		loop_rate.sleep();	
+		i++;
 	}
 	
 	
